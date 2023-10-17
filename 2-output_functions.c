36875e6d46097318dd306c290ae83936a47acfa8
eazytbay <eazytbay@gmail.com>
2023-10-17 15:05:33 -0700

    2-output_functions.c corrections

--- 2-output_functions.c
+++ 2-output_functions.c
@@ -1,23 +1,23 @@
 #include "main.h"
 /**
- * gen_custom_pointer - Prints the value of a pointer variable
- * @types: List of arguments
+ * gen_custom_pointer -A function that displays the value of a pointer variable
+ * @args: List of arguments
  * @buffer: Buffer array for printing
  * @flags: Active flags
  * @width: Width
- * @precision: Precision specification
- * @size: Size specifier
+ * @precision: specified precision
+ * @size: specified size
  * Return: Number of characters printed
  */
-int gen_custom_pointer(va_list types, char buffer[],
-int flags, int width, int precision, int size)
+int gen_custom_pointer(va_list args, char buffer[], int flags,
+int width, int precision, int size)
 {
 char extra_char = 0, padding_char = ' ';
 int index = BUFFER_SIZE - 2, length = 2, padding_start
 = 1; /* Length=2 for '0x' */
 unsigned long address;
 char map_to[] = "0123456789abcdef";
-void *ptr = va_arg(types, void *);
+void *ptr = va_arg(args, void *);
 UNUSED_VAR(width);
 UNUSED_VAR(size);
 if (ptr == NULL)
@@ -42,37 +42,37 @@ return (add_custom_pointer(buffer, index, length,
 width, flags, padding_char, extra_char, padding_start));
 }
 /**
- * gen_custom_non_printable - Prints ASCII codes in
+ * gen_custom_non_printable -  A function that Prints ASCII codes in
  * hexadecimal of non-printable characters
  * @types: List of arguments
  * @buffer: Buffer array for printing
  * @flags: Active flags
  * @width: Width
- * @precision: Precision specification
- * @size: Size specifier
- * Return: Number of characters printed
+ * @precision: specified precision
+ * @size: specified size
+ * Return: Total printed characters
  */
 int gen_custom_non_printable(va_list types, char buffer[],
 int flags, int width, int precision, int size)
 {
-int i = 0, offset = 0;
-char *str = va_arg(types, char *);
+int x = 0, o = 0;
+char *ptr = va_arg(types, char *);
 UNUSED_VAR(flags);
 UNUSED_VAR(width);
 UNUSED_VAR(precision);
 UNUSED_VAR(size);
-if (str == NULL)
+if (ptr == NULL)
 return (write(1, "(null)", 6));
-while (str[i] != '\0')
+while (ptr[x] != '\0')
 {
-if (custom_print(str[i]))
-buffer[i + offset] = str[i];
+if (custom_print(ptr[x]))
+buffer[x + o] = ptr[x];
 else
-offset += add_custom_hexa_code(str[i], buffer, i + offset);
-i++;
+o += add_custom_hexa_code(ptr[x], buffer, x + o);
+x++;
 }
-buffer[i + offset] = '\0';
-return (write(1, buffer, i + offset));
+buffer[x + o] = '\0';
+return (write(1, buffer, x + o));
 }
 /**
  * gen_custom_reverse - Prints a reversed string.
@@ -87,27 +87,27 @@ return (write(1, buffer, i + offset));
 int gen_custom_reverse(va_list types, char buffer[],
 int flags, int width, int precision, int size)
 {
-char *str;
-int i, count = 0;
+char *ptr;
+int x, cnt = 0;
 UNUSED_VAR(buffer);
 UNUSED_VAR(flags);
 UNUSED_VAR(width);
 UNUSED_VAR(size);
-str = va_arg(types, char *);
-if (str == NULL)
+ptr = va_arg(types, char *);
+if (ptr == NULL)
 {
 UNUSED_VAR(precision);
-str = ")Null(";
+ptr = ")Null(";
 }
-for (i = 0; str[i]; i++)
+for (x = 0; ptr[x]; x++)
 ;
-for (i = i - 1; i >= 0; i--)
+for (x = x - 1; x >= 0; x--)
 {
-char character = str[i];
+char character = ptr[x];
 write(1, &character, 1);
-count++;
+cnt++;
 }
-return (count);
+return (cnt);
 }
 /**
  * gen_custom_rot13string - A function that Print a string in ROT13.
